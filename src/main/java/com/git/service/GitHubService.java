457ba28ace8d6df6e4051508e07fa8049a3ce46d
Leonid <leonidg34@gmail.com>
2024-07-29 12:39:47 +0200

    git api get specific user repository

--- src/main/java/com/git/service/GitHubService.java
+++ src/main/java/com/git/service/GitHubService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface GitHubService {
     List<GitRepository> getUserRepo(String username);
+    GitRepository getUserSpecificRepo(String username, String repoName);
 }
